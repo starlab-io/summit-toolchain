a0e6de77f904e04090fe7ef87db3f39d0ad28365
Alex Speasmaker <alex.speasmaker@gmail.com>
2018-10-11 14:43:57 -0500

    Found a way to make threaddable function do what I want, now just have to make a thread out of it

--- test/telegraf-xen.py
+++ test/telegraf-xen.py
@@ -1,15 +1,11 @@
-#
-#Taken from: https://gist.github.com/ernetas/b14ad803162baf7d7635a8d8b31b8d64
-#
-
-
-
-
 #!/usr/bin/env python3
+
 import socket
 hostname = socket.gethostname()
 import subprocess
 import time
+
+
 
 def print_influx_data(xendomain, item, value):
   global timestamp
@@ -19,35 +15,30 @@ def print_influx_data(xendomain, item, value):
 def get_xentop_lines():
   # Using -i1 does not show correct CPU usage
   # Since collecting data is slow, we must run xentop only once and collect all the needed data from one run
-  p = subprocess.Popen(['xentop', '-b', '-f', '-i2'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-  out, _ = p.communicate(timeout=60)
-  out = out.decode('utf-8')
+  
+  p = subprocess.Popen(['xentop', '-b', '-f', '-d 1','-i12'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
   # Ignore the first iteration, as data there is not correct (e.g. CPU usage 0%)
   # Start reading lines only after second line starting with 'NAME' (second iteration)
-  iteration = 0
   
-  xentop = []
+  while p.poll() is None:
+    line = p.stdout.readline()
+    print( line )
+
+
+    
    
-  for line in out.splitlines():
-    if iteration < 2 and line.strip().startswith('NAME'):
-      iteration += 1
-    elif iteration == 2 and not line.strip().startswith('NAME'):
-      xentop.append(line.strip())
-  
-
-  for line in xentop:
-    domain = line.split()
-#    print_influx_data(domain[0],'cpu_time','cpu_time=' + domain[2] + 'i')
-#    print_influx_data(domain[0],'cpu','load=' + domain[3])
-    print_influx_data(domain[0],'cpu','load=' + domain[3])
-#    print_influx_data(domain[0],'mem','kb=' + domain[4] + 'i')
-#    print_influx_data(domain[0],'nettx','nettx=' + domain[10] + 'i')
-#    print_influx_data(domain[0],'netrx','netrx=' + domain[11] + 'i')
-#    print_influx_data(domain[0],'disk_rd','disk_rd=' + domain[14] + 'i')
-#    print_influx_data(domain[0],'disk_wr','disk_wr=' + domain[15] + 'i')
+#    for line in xentop:
+#      domain = line.split()
+#      print_influx_data(domain[0],'cpu_time','cpu_time=' + domain[2] + 'i')
+#      print_influx_data(domain[0],'cpu','load=' + domain[3])
+#      print_influx_data(domain[0],'cpu','load=' + domain[3])
+#      print_influx_data(domain[0],'mem','kb=' + domain[4] + 'i')
+#      print_influx_data(domain[0],'nettx','nettx=' + domain[10] + 'i')
+#      print_influx_data(domain[0],'netrx','netrx=' + domain[11] + 'i')
+#      print_influx_data(domain[0],'disk_rd','disk_rd=' + domain[14] + 'i')
+#      print_influx_data(domain[0],'disk_wr','disk_wr=' + domain[15] + 'i')
 
 
-timestamp = str("%.9f" % time.time()).replace('.', '') # A little bit more exact than doing this for each xentop line
 xentop_lines=get_xentop_lines()
 
 
